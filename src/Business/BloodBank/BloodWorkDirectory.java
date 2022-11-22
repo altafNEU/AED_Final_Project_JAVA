312af4be661f91522575433db6cdeeac6e22683b
Altaf Shaikh <shaikh.alt@northeastern.edu>
2022-11-21 21:26:48 -0500

    bloodwork directory

--- src/Business/BloodBank/BloodWorkDirectory.java
+++ src/Business/BloodBank/BloodWorkDirectory.java
@@ -9,5 +9,27 @@ package Business.BloodBank;
  * @author altaf
  */
 public class BloodWorkDirectory {
+     
+    // Added blood work directory for the list
+    ArrayList<BloodWork> bloodWorkList = new ArrayList<BloodWork>();
+
+    // Blood work getter and setter 
+    public ArrayList<BloodWork> getBloodWorkList() {
+        return bloodWorkList;
+    }
+
+    public void setBloodWorkList(ArrayList<BloodWork> bloodWorkList) {
+        this.bloodWorkList = bloodWorkList;
+    }
+    
+    // Add item to the blood work list
+    public void addBloodWork(BloodWork item) {
+        bloodWorkList.add(item);
+    }
+    
+    // removing the blood work from the blood work list to safe storage
+    public void removeBloodWork(BloodWork item) {
+        bloodWorkList.remove(item);
+    }
     
 }
