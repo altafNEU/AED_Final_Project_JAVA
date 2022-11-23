3dd4f386704df8dab8465c7133202c786ad78200
Satyajit Das <das.sat@northeastern.edu>
2022-11-23 09:36:28 -0500

    Emergency Police model creation- Police

---
+++ src/EmergencyPolice/Police.java
@@ -0,0 +1,69 @@
+package EmergencyPolice;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author satyajitdas
+ */
+public class Police {
+    String name;
+    String Location;
+    String phonenumber;
+    String userid;
+    String password;
+
+    public Police(String name, String Location, String phonenumber, String userid, String password) {
+        this.name = name;
+        this.Location = Location;
+        this.phonenumber = phonenumber;
+        this.userid = userid;
+        this.password = password;
+        setUserName(userid);
+        setPwd(password);
+        setRole(new Police_Officer());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String Location) {
+        this.Location = Location;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
