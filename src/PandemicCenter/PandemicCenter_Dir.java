f2d2e803b5ee929928bfda0c5337ecb2efa8caa5
aziz-voh <vohra.az@northeastern.edu>
2022-11-23 16:34:35 -0500

    Created directory for pasndemic centre

---
+++ src/PandemicCenter/PandemicCenter_Dir.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PandemicCenter;
+
+/**
+ *
+ * @author azizvohra
+ */
+public class PandemicCenter_Dir {
+     ArrayList<PandemicCenter> pandemicdirectory =new ArrayList<>();
+
+    public ArrayList<PandemicCenter> getPandemicdirectory() {
+        return pandemicdirectory;
+    }
+
+    public void setPandemicdirectory(ArrayList<PandemicCenter> pandemicdirectory) {
+        this.pandemicdirectory = pandemicdirectory;
+    }
+
+    public void addPandemicCenter(PandemicCenter center)
+    {        
+       pandemicdirectory.add(center);
+    }
+    
+    public void removePandemicCenter(PandemicCenter customer){
+        pandemicdirectory.remove(customer);
+    }
+}
