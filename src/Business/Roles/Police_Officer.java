eb8d44e5ad307a16b035b9063096f310e1d3bf57
Altaf Shaikh <shaikh.alt@northeastern.edu>
2022-11-25 08:41:00 -0500

    added more business roles, issues resolvde of previous files

---
+++ src/Business/Roles/Police_Officer.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Roles;
+
+import Business.EcoSystem;
+import Business.UserAcc.UserAcc;
+import UserInterface.user.UserAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author altaf
+ */
+public class Police_Officer {
+
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAcc account, EcoSystem business) {
+        return new UserAreaJPanel(userProcessContainer, account, business);
+    }
+}
