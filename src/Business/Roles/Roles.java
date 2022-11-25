85511e9d4a86ef0ee74bd3f4befaaa3cc37322e9
Altaf Shaikh <shaikh.alt@northeastern.edu>
2022-11-25 08:56:45 -0500

    business roles fixed issues, added more role files

---
+++ src/Business/Roles/Roles.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Roles;
+
+import Business.EcoSystem;
+
+import Business.UserAcc.UserAcc;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author altaf
+ */
+public class Roles {
+
+    public abstract JPanel createWorkArea(JPanel userProcessContainer,
+            UserAcc account,
+            EcoSystem business);
+
+    @Override
+    public String toString() {
+        return this.getClass().getName();
+    }
+
+    public enum TypeOfRole {
+
+        Admin_Pharmacy("AdminPharmacy"),
+        User("User"),
+        Admin_Vaccination("AdminVaccination"),
+        Doctor("Doctor"),
+        Admin_BloodBank("AdminBloodbank"),
+        Admin_PandemicTestCenter("AdminPandemicTesting"),
+        System_Admin("SysAdmin");
+
+        private String value;
+
+        private TypeOfRole(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+}
---
+++ src/Business/Roles/System_Admin.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Roles;
+
+import Business.EcoSystem;
+import Business.UserAcc.UserAcc;
+import UserInterface.SysAdmin.SysAdminWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author altaf
+ */
+public class System_Admin {
+     public JPanel createWorkArea(JPanel userProcessContainer, UserAcc account, EcoSystem system) {
+        return new SysAdminWorkAreaJPanel(userProcessContainer, system);
+    }
+}
