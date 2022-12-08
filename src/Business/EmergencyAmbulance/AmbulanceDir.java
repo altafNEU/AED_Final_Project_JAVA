5fbc7b4a30125e4392cb8ed65e088f8766ca1385
Altaf Shaikh <shaikh.alt@northeastern.edu>
2022-12-08 10:19:09 -0500

    ambulance dir in business model updated

--- src/Business/EmergencyAmbulance/AmbulanceDir.java
+++ src/Business/EmergencyAmbulance/AmbulanceDir.java
@@ -3,13 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.EmergencyAmbulance;
+
 import java.util.ArrayList;
+
 /**
  *
  * @author altaf
  */
 public class AmbulanceDir {
-          ArrayList<Ambulance> ambulances =new ArrayList<Ambulance>();
+
+    ArrayList<Ambulance> ambulances = new ArrayList<Ambulance>();
 
     public ArrayList<Ambulance> getAmbulances() {
         return ambulances;
@@ -18,12 +21,12 @@ public class AmbulanceDir {
     public void setAmbulances(ArrayList<Ambulance> ambulances) {
         this.ambulances = ambulances;
     }
-            
-       public void addNewAmbulance(Ambulance newAmbulance)
-    {        
-       ambulances.add(newAmbulance);
+
+    public void addNewAmbulance(Ambulance newAmbulance) {
+        ambulances.add(newAmbulance);
     }
-        public void removeAmbulance(Ambulance ambulance){
+
+    public void removeAmbulance(Ambulance ambulance) {
         ambulances.remove(ambulance);
     }
 }
